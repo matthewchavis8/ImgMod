0b58cad8e131a939e61772914d54577a8154cce2
matthewchavis8 <matthewchavis8@gmail.com>
2025-03-23 16:55:41 -0500

    (feat) added decoding for cli

--- src/commands.rs
+++ src/commands.rs
@@ -1,9 +1,10 @@
-use crate::args::EncodeArgs;
+use crate::args::{DecodeArgs, EncodeArgs};
 use crate::png::Png;
 use crate::chunk::Chunk;
 
 pub enum CommandError{
-    InvalidEncode
+    InvalidEncode,
+    InvalidDecode,
 }
 
 pub fn encode(args: &EncodeArgs) -> Result<(),  CommandError> {
@@ -17,3 +18,15 @@ pub fn encode(args: &EncodeArgs) -> Result<(),  CommandError> {
         png.write_file(&args.file_path).map_err(|_| CommandError::InvalidEncode)
     }
 }
+
+pub fn decode(args: &DecodeArgs) -> Result<(), CommandError> {
+    let png = Png::from_file(&args.file_path).unwrap();
+    
+    if let Some(chunk) = png.chunk_by_type(&args.chunk_type) {
+        println!("msg: {}", chunk.data_as_string().unwrap());
+        Ok(())
+    } else {
+        Err(CommandError::InvalidDecode)
+    }
+}
+
