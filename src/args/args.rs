bcefb5c98c0078b16f229b8cc221c5895ee85b32
matthewchavis8 <matthewchavis8@gmail.com>
2025-03-31 15:35:26 -0500

    (feat) added in a manage mode that will support conversion of image types in the cli. Also can delete images in your folder, and look up images from the internet

--- src/args/args.rs
+++ src/args/args.rs
@@ -14,6 +14,7 @@ pub enum Commands {
     Decode(DecodeArgs),
     Remove(RemoveArgs),
     Print(PrintArgs),
+    Manage(ManageArgs)
 }
 
 #[derive(Debug, Clone, Args)]
@@ -39,4 +40,44 @@ pub struct RemoveArgs {
 #[derive(Debug, Clone, Args)]
 pub struct PrintArgs {
     pub file_path: PathBuf
+}
+
+
+#[derive(Debug, Parser)]
+pub struct ManageArgs {
+    #[clap(subcommand)]
+    pub manage_command: ManageCommands,
+
+}
+
+#[derive(Debug, Subcommand)]
+pub enum ManageCommands {
+    Delete(DeleteArgs),
+    DownLoadFromInternet(DownloadFromInternetArgs),
+    Convert(ConvertArgs),
+}
+
+#[derive(Debug, Clone, Args)]
+pub struct DeleteArgs {
+    pub file_path: PathBuf
+}
+
+#[derive(Debug, Clone, Args)]
+pub struct DownloadFromInternetArgs {}
+
+#[derive(Debug, Clone, Args)]
+pub struct ConvertArgs {
+    #[arg(short = 'p')]
+    pub convert_to_png: bool,
+
+    #[arg(short = 'j')]
+    pub convert_to_jpg: bool,
+
+    #[arg(short = 't')]
+    pub convert_to_tiff: bool,
+
+    #[arg(short = 'w')]
+    pub convert_to_webp: bool,
+
+    pub file_path: PathBuf,
 }
