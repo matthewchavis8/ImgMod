f55c9f9e83007180da7a51420c758ba2d1f40b12
matthewchavis8 <matthewchavis8@gmail.com>
2025-04-04 21:13:11 -0500

    (test, fix) broke the unit test lmfao but fixed it and added unit test for converting from png to jpeg

--- tests/test_cli.rs
+++ tests/test_cli.rs
@@ -12,28 +12,28 @@ mod tests {
         pub fn remove_chunks() {
             let mut cmd = Command::cargo_bin("ImgMod")
                 .unwrap();
-            cmd.args(&["remove", "./images/test_image.png", "maTt"]);
+            cmd.args(["remove", "./images/test_image.png", "maTt"]);
             cmd.assert().success();
         }
 
         pub fn print_chunks() -> assert_cmd::assert::Assert {
             let mut cmd = Command::cargo_bin("ImgMod")
                 .unwrap();
-            cmd.args(&["print", "./images/test_image.png"]);
+            cmd.args(["print", "./images/test_image.png"]);
             cmd.assert()
         }
 
         pub fn encode() {
             let mut cmd = Command::cargo_bin("ImgMod")
             .unwrap();
-            cmd.args(&["encode", "./images/test_image.png", "maTt", "Hello Matt!"]);   
+            cmd.args(["encode", "./images/test_image.png", "maTt", "Hello Matt!"]);   
             cmd.assert().success();
         }
 
         pub fn decode() {
             let mut cmd = Command::cargo_bin("ImgMod")
             .unwrap();
-            cmd.args(&["decode", "./images/test_image.png", "maTt"]);   
+            cmd.args(["decode", "./images/test_image.png", "maTt"]);   
             
             cmd.assert()
                 .stdout("msg: Hello Matt!\n");
@@ -45,7 +45,7 @@ mod tests {
         
             let mut cmd = Command::cargo_bin("ImgMod")
             .unwrap();
-            cmd.args(&["manage", "download", &url, "test.png"]);
+            cmd.args(["manage", "download", &url, "test.png"]);
             cmd.assert().success();
         }
     }
@@ -55,12 +55,10 @@ mod tests {
     fn test_print() {
         let assert = TestCli::print_chunks();
         assert.stdout(
-  "File: ./images/test_image.png, Size: 231668
+  "File: ./images/test_image.png, Size: 261999
   chunk#0{ chunk_type: IHDR, data_length: 13}
-  chunk#1{ chunk_type: pHYs, data_length: 9}
-  chunk#2{ chunk_type: iTXt, data_length: 1891}
-  chunk#3{ chunk_type: IDAT, data_length: 229687}
-  chunk#4{ chunk_type: IEND, data_length: 0}\n"
+  chunk#1{ chunk_type: IDAT, data_length: 261942}
+  chunk#2{ chunk_type: IEND, data_length: 0}\n"
         );
     }
 
@@ -71,13 +69,11 @@ mod tests {
         let check_chunks_after_chunk_added = |()| {
             let assert = TestCli::print_chunks();
             assert.stdout(
-  "File: ./images/test_image.png, Size: 231691
+  "File: ./images/test_image.png, Size: 262022
   chunk#0{ chunk_type: IHDR, data_length: 13}
-  chunk#1{ chunk_type: pHYs, data_length: 9}
-  chunk#2{ chunk_type: iTXt, data_length: 1891}
-  chunk#3{ chunk_type: IDAT, data_length: 229687}
-  chunk#4{ chunk_type: IEND, data_length: 0}
-  chunk#5{ chunk_type: maTt, data_length: 11}\n"
+  chunk#1{ chunk_type: IDAT, data_length: 261942}
+  chunk#2{ chunk_type: IEND, data_length: 0}
+  chunk#3{ chunk_type: maTt, data_length: 11}\n"
             );
         };
         TestCli::encode();
@@ -110,7 +106,7 @@ mod tests {
     
         let mut cmd = Command::cargo_bin("ImgMod")
         .unwrap();
-        cmd.args(&["manage", "delete", file_path.to_str().unwrap()]);
+        cmd.args(["manage", "delete", file_path.to_str().unwrap()]);
         cmd.assert().success();
 
         assert!(!file_path.exists());
@@ -123,7 +119,7 @@ mod tests {
         
         let mut cmd = Command::cargo_bin("ImgMod")
         .unwrap();
-        cmd.args(&["manage", "delete", "images/test.png"]);
+        cmd.args(["manage", "delete", "images/test.png"]);
         cmd.assert().success();
 
         let check_path = PathBuf::from("images/test.png");
@@ -132,12 +128,12 @@ mod tests {
 
     #[test]
     #[serial]
-    fn test_convert_image() {
+    fn test_convert_image_to_png() {
         TestCli::install_image_from_the_internet();
         
         let mut cmd = Command::cargo_bin("ImgMod")
         .unwrap();
-        cmd.args(&["manage", "convert", "-j", "images/test.png"]);
+        cmd.args(["manage", "convert", "-j", "images/test.png"]);
         cmd.assert().success();
 
         let check_path = PathBuf::from("images/test.jpeg");
@@ -145,7 +141,29 @@ mod tests {
 
         let mut cmd = Command::cargo_bin("ImgMod")
         .unwrap();
-        cmd.args(&["manage", "delete", "images/test.jpeg"]);
+        cmd.args(["manage", "delete", "images/test.jpeg"]);
+        cmd.assert().success();
+
+        assert!(!check_path.exists());
+
+    }
+
+    #[test]
+    #[serial]
+    fn test_convert_image_to_jpeg() {
+        TestCli::install_image_from_the_internet();
+        
+        let mut cmd = Command::cargo_bin("ImgMod")
+        .unwrap();
+        cmd.args(["manage", "convert", "-j", "images/test.png"]);
+        cmd.assert().success();
+
+        let check_path = PathBuf::from("images/test.jpeg");
+        assert!(check_path.exists());
+
+        let mut cmd = Command::cargo_bin("ImgMod")
+        .unwrap();
+        cmd.args(["manage", "delete", "images/test.jpeg"]);
         cmd.assert().success();
 
         assert!(!check_path.exists());
